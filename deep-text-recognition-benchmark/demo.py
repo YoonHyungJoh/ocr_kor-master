7b3668892fec00a11dada5187a0407fd28165268
YoonHyungJoh <69350072+YoonHyungJoh@users.noreply.github.com>
2020-08-19 20:30:02 +0900

    Update demo.py

--- deep-text-recognition-benchmark/demo.py
+++ deep-text-recognition-benchmark/demo.py
@@ -32,7 +32,7 @@ def demo(opt):
 
     # load model
     print('loading pretrained model from %s' % opt.saved_model)
-    model.load_state_dict(torch.load(opt.saved_model, map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load(opt.saved_model))
 
     # prepare data. two demo images from https://github.com/bgshih/crnn#run-demo
     AlignCollate_demo = AlignCollate(imgH=opt.imgH, imgW=opt.imgW, keep_ratio_with_pad=opt.PAD)
